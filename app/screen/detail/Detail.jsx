276db175500a085450241241ca2be6fc380d6b8f
roma29733@gmail.com <roma29734@gmail.com>
2023-11-17 22:09:56 +0300

    modern detail Screen

--- app/screen/detail/Detail.jsx
+++ app/screen/detail/Detail.jsx
@@ -1,8 +1,9 @@
-import {View, Text, Image, Platform, Button, Alert, TouchableOpacity} from "react-native";
+import {View, Text, Image, Platform, Button, Alert, TouchableOpacity, Linking} from "react-native";
 import styles from "./detail.style"
 import {Stack, useLocalSearchParams} from "expo-router";
 import * as MediaLibrary from 'expo-media-library';
-import * as Permissions from 'expo-permissions';
+import {Camera} from "expo-camera";
+import {useEffect} from "react";
 
 const Detail = () => {
 
@@ -10,11 +11,14 @@ const Detail = () => {
 
 
     const savePhotoToGallery = async () => {
-        const {status} = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+        const {status} = await Camera.getCameraPermissionsAsync();
+
         if (status !== 'granted') {
             console.log('Permission not granted!');
+            Alert.alert("erar", `Permission not granted! ${status}`, "pizdech", "eban");
             return;
         }
+
         const photoUri = 'https://images.unsplash.com/photo-1682687219356-e820ca126c92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzOTg3Nzh8MXwxfGFsbHwxfHx8fHx8Mnx8MTcwMDE2NDY0MHw&ixlib=rb-4.0.3&q=80&w=1080';
 
         const asset = await MediaLibrary.createAssetAsync(photoUri);
@@ -37,16 +41,11 @@ const Detail = () => {
                 }}
             />
             <Image source={{uri: params.Detail}} style={styles.image}/>
-            {/*<Button title='Download' onPress={async () => {*/}
-            {/*    // await MediaLibrary.getPermissionsAsync()*/}
-            {/*    // await MediaLibrary.saveToLibraryAsync(`file:///mnt/sdcard/photo.png/${params.Detail}`,);*/}
-            {/*}}/>*/}
-            <Button title='Awloda' onPress={
-                savePhotoToGallery
-            }/>
-            {/*<TouchableOpacity style={styles.buttonDownload}>*/}
-            {/*    <Image source={require('../../../assets/ic_download.svg')} style={styles.imageDownload}/>*/}
-            {/*</TouchableOpacity>*/}
+
+            <TouchableOpacity style={styles.buttonDownload} onPress={savePhotoToGallery}>
+                {/*<Image source={require('../../../assets/ic_download.svg')} style={styles.imageDownload}/>*/}
+                <Text style={styles.textDownload}>Download</Text>
+            </TouchableOpacity>
 
 
         </View>
--- app/screen/detail/detail.style.js
+++ app/screen/detail/detail.style.js
@@ -1,22 +1,32 @@
 import {StyleSheet} from "react-native";
 
-const styles=  StyleSheet.create({
+const styles = StyleSheet.create({
     image: {
         flex: 1,
         height: 350,
         flexDirection: 'column',
         marginHorizontal: 12,
         marginVertical: 12,
+        borderRadius: 16,
     },
     container: {
         width: '100%',
         height: '100%',
     },
     buttonDownload: {
-        width: 48,
+        width: 120,
         height: 48,
+        marginBottom: 16,
+        alignSelf: 'center',
+        marginHorizontal: 'auto',
         borderRadius: 32,
         backgroundColor: '#000000'
+    },
+    textDownload: {
+        fontSize: 16,
+        top: '25%',
+        textAlign: 'center',
+        color: '#fff',
     },
     imageDownload: {
         height: 48,
